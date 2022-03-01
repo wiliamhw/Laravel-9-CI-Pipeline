9a3df1b81b6dc8876525cbc3ecb3b6beca4f9f7c
William Handi Wijaya <52129348+wiliamhw@users.noreply.github.com>
2022-03-01 18:55:44 +0700

    Fix #3: add facade example (#6)
    * feat: add sentry dependencies
    
    * feat: add facade example

---
+++ app/Supports/FacadeExample.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Supports;
+
+class FacadeExample
+{
+    /**
+     * Test facade.
+     *
+     * @return string
+     */
+    public function test(): string
+    {
+        return "Hello, world!";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
 });
+
+Route::get('/test-facade', function () {
+    return FacadeExample::test();
+});
